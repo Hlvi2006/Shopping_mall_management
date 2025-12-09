826ab8501d763120c529202f0caa7fc0766f77c1
Ulvi <168309465+Hlvi2006@users.noreply.github.com>
2025-12-09 16:17:24 +0400

    Delete Shopping_mall/.vscode directory

--- Shopping_mall/.vscode/src/models/entity.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Entity(ABC):
-    """Abstract base class for entities, demonstrating abstraction."""
-    @abstractmethod
-    def to_dict(self):
-        pass
